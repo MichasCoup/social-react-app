b6f89bd154cdd564a54ef4979ad62d2254a56578
Michael Flach <vuti@gmx.de>
2019-02-05 12:19:30 +0100

    Initial express server with route files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const mongoose = require('mongoose');
+
+
+// Load routes
+const users = require('./routes/api/users');
+const profil = require('./routes/api/profil');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true }).then(()=> console.log('MongoDB connected')).catch((e) => console.log(e));
+
+app.get('/', (req, res) => res.send('Hello react!'));
+
+
+// User routes
+app.use('/api/users', users);
+app.use('/api/profil', profil);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
