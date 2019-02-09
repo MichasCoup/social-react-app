dfdfdc9dc8dece1e236bed56f6d4b08a24496206
Michael Flach <vuti@gmx.de>
2019-02-09 02:27:13 +0100

    Add User Login and Logout with Redux

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
-// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {logoutUser} from "../../actions/authActions";
 
 class Navbar extends Component {
+
+	onLogoutClick (e) {
+		e.preventDefault();
+		this.props.logoutUser();
+	}
+
 	render() {
+
+		const { isAuthenticated, user } = this.props.auth;
+
+		const authLinks = (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item">
+					{/*<Link className="nav-link" to="register">Sign Up</Link>*/}
+				{/*</li>*/}
+				{/*<li className="nav-item">*/}
+					{/*<Link className="nav-link" to="login">Login</Link>*/}
+					<a href="/" onClick={this.onLogoutClick.bind(this)} className="nav-link">
+						Logout
+						<img src={user.avatar} alt={user.name} className={"rounded-circle"} style={{width: '25px', margin:'5px'}} title="You must have a Gravatar connected to your email to display an image"/>
+					</a>
+
+				</li>
+			</ul>
+		);
+
+		const guestLinks = (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item">
+					<Link className="nav-link" to="register">Sign Up</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="login">Login</Link>
+				</li>
+			</ul>
+		);
+
 		return (
 			<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
 				<div className="container">
-					<a className="navbar-brand" href="landing.html">DevConnector</a>
+					<Link className="navbar-brand" to="/">DevConnector</Link>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
 						<span className="navbar-toggler-icon"></span>
 					</button>
@@ -14,19 +53,15 @@ class Navbar extends Component {
 					<div className="collapse navbar-collapse" id="mobile-nav">
 						<ul className="navbar-nav mr-auto">
 							<li className="nav-item">
-								<a className="nav-link" href="profiles.html"> Developers
-								</a>
+								<Link className="nav-link" to="profiles">{' '} Developers
+								</Link>
 							</li>
 						</ul>
 
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item">
-								<a className="nav-link" href="register.html">Sign Up</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="login.html">Login</a>
-							</li>
-						</ul>
+						{/*Links*/}
+						{isAuthenticated ? authLinks : guestLinks}
+
+
 					</div>
 				</div>
 			</nav>
@@ -34,6 +69,13 @@ class Navbar extends Component {
 	}
 }
 
-// MyComponent.propTypes = {};
+Navbar.propTypes = {
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
